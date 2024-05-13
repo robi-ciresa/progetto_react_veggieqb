5ea277abbf86e9c2861314674db43e59dddd8c24
robi-ciresa <robi.ciresa.w@gmail.com>
2024-05-13 11:05:55 +0200

    Last_version

--- src/assets/pages/jsx/Detailpage.jsx
+++ src/assets/pages/jsx/Detailpage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-import helmet from "helmet";
 import Navbar from "../../components/jsx/Navbar";
 import Footer from "../../components/jsx/Footer";
 import Errorpage from "./Errorpage";
